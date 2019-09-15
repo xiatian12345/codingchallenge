675f03ffff1734ecb722cdad19a33ed67eb54373
kangchao <kangchao@100tal.com>
2019-09-15 09:49:41 +0800

    upload lesson1

---
+++ lesson1/main.js
@@ -0,0 +1,24 @@
+/**
+ * 宇宙星空
+ */
+
+const starsCount = 100;
+const stars = [];
+window.speed = 0;
+
+function setup() {
+  createCanvas(800, 600);
+  for (let i = 0; i < starsCount; i++) {
+    stars[i] = new Star();
+  }
+}
+
+function draw() {
+  window.speed = map(mouseX, 0, width, -10, 10);
+  background(147, 145, 160, 40);
+  translate(width / 2, height / 2);
+  for (let i = 0; i < stars.length; i++) {
+    stars[i].update();
+    stars[i].show();
+  }
+}
